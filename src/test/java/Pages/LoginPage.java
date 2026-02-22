41393e6df89ceeac692b5fd2a755f364eafbffd7
sintutu <160270685+sintutu@users.noreply.github.com>
2026-02-22 23:04:43 +0200

    Homework 20260223

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,9 +1,19 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
+    private final WebDriverWait wait;
+
+    public LoginPage(WebDriver driver){
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
 
     @FindBy(xpath = "//*[@id='app-root']/nav/div[1]/div[3]/button/span[2]")
     WebElement loginButton;
@@ -19,6 +29,33 @@ public class LoginPage {
 
     @FindBy(xpath = "//h2[contains(text(), 'Welcome back')]")
     WebElement welcomeBackMessage;
+
+    // Chose id since id is unique element in html document
+    @FindBy(id = "signup-toggle")
+    WebElement signUpButton;
+
+    // Chose css selector because I wanted aria-label attribute "Show password"
+    // Chose to filter on button with that aria-label just in case another element is given the same aria label.
+    // Drawback is likelihood that element button is changed to a div element with role="button"
+    // Chose to go specific, so I catch change in DOM and trust tests, rather than debug duplicate aria-labels.
+    @FindBy(css = "button[aria-label='Show password']")
+    WebElement showPasswordButton;
+
+    // Chose xpath not because there weren't any better locators.
+    // I wanted to practice using the Elements window in the browser.
+    // I chose normalize-space() to avoid any issues with extra spaces in the text.
+    @FindBy(xpath = "//button[normalize-space()='Forgot Password?']")
+    WebElement forgotPasswordLink;
+
+    // Chose xpath just to use contains
+    // Learned that . concatenates text from all child elements,
+    //     so I can use contains to avoid any issues with extra spaces or emojis in the text.
+    @FindBy(xpath = "//h2[contains(.,'Login to Access Learning Materials')]")
+    WebElement loginHeading;
+
+    // Chose class name because the homework wanted class name used
+    @FindBy(className = "nav-items")
+    WebElement navItems;
 
     public void clickLoginButton() {
         loginButton.click();
@@ -42,5 +79,30 @@ public class LoginPage {
         if (!actualMessage.equals(expectedMessage)) {
             throw new AssertionError("Expected message: " + expectedMessage + ", but got: " + actualMessage);
         }
+    }
+
+    public boolean isSignUpVisible() {
+        return wait.until(ExpectedConditions.visibilityOf(signUpButton)).isDisplayed();
+    }
+
+    public boolean isShowPasswordVisible() {
+        return wait.until(ExpectedConditions.visibilityOf(showPasswordButton)).isDisplayed();
+    }
+
+    public boolean isForgotPasswordVisible() {
+        return wait.until(ExpectedConditions.visibilityOf(forgotPasswordLink)).isDisplayed();
+    }
+
+    public boolean isLoginHeadingVisible() {
+        return wait.until(ExpectedConditions.visibilityOf(loginHeading)).isDisplayed();
+    }
+
+    public boolean isNavigationBarVisible() {
+        return wait.until(ExpectedConditions.visibilityOf(navItems)).isDisplayed();
+    }
+
+    // Gets content of navigation bar and places them on one line
+    public String getNavigationBarText() {
+        return wait.until(ExpectedConditions.visibilityOf(navItems)).getText().replace('\n', ' ').trim();
     }
 }
--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,12 +1,13 @@
 package Tests;
 
 import Base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
 
-    @Test
+    @Test(priority = 1)
     public void loginWithValidDetails() throws InterruptedException {
 
         loginPage.clickLoginButton();
@@ -17,5 +18,37 @@ public class LoginTest extends BaseTest {
         loginPage.clickSubmitButton();
         Thread.sleep(3000);
         loginPage.verifyLoginSuccess("Welcome back, Avela! \uD83D\uDC4B");
+
+    }
+
+    @Test
+    public void signUpButtonShouldBeVisible() {
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.isSignUpVisible(), "Sign Up button should be visible on the login page.");
+    }
+
+    @Test
+    public void showPasswordButtonShouldBeVisible() {
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.isShowPasswordVisible(), "Show password button should be visible on the login page.");
+    }
+
+    @Test
+    public void forgotPasswordLinkShouldBeVisible() {
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.isForgotPasswordVisible(), "Forgot password link should be visible on the login page.");
+    }
+
+    @Test
+    public void loginHeadingShouldBeVisible() {
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.isLoginHeadingVisible(), "Login heading should be visible on the login page.");
+    }
+
+    @Test
+    public void navigationBarShouldBeVisible() {
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.isNavigationBarVisible(), "Navigation bar should be visible on the login page.");
+        Assert.assertEquals(loginPage.getNavigationBarText(),"\uD83C\uDFE0 Home \uD83D\uDCD6 About Us ⭐ Testimonials \uD83D\uDCDA Learn ▼ \uD83D\uDC65 Community ▼ \uD83D\uDD17 Connect ▼");
     }
 }
